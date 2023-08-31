68753e251292b5cb65c3f41d57dd579a94ecdad0
GilangPradnyana <gilangpradnyana2@gmail.com>
2023-08-31 21:01:55 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -4,10 +4,14 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Connect to github</title>
+  <link rel="stylesheet" href="style.css">
 </head>
 <body>
   <h1>Connect to github</h1>
-  
+  <ul>
+    <li>Satu</li>
+    <li>Dua</li>
+  </ul>
   
 </body>
 </html>
